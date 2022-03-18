2d8ad140f1133c16d84adc8ebccebc412d79a517
ConalG <conal-glynnmartin@hotmail.com>
2022-03-18 23:07:23 +0000

    Update to Script.js
    Function created for implementing credits and result

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let credits = 100;
 let jackpot = 0;
+let result = false;
+
 //Genertate Random Number
 const generateRandomNumber = () =>{
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * 2);
   }
 
 //Get Element ID
@@ -15,14 +17,14 @@ const updateCredits = () =>{
     const value = credits;
     money.innerHTML = `${value}`;
 }
-
+//Update Display for Jackpot
 const updateJackpot = () =>{
     const prize = getElement('prize');
     const value = jackpot;
     prize.innerHTML = `${value}`;
 }
 
-//Play Function
+//Main Functionality of Game
 const playGame = () =>{
     let slot1 = getElement(`slot1`);
     let slot2 = getElement(`slot2`);
@@ -39,15 +41,42 @@ const playGame = () =>{
     slot3.className = "c"+num3;
     slot4.className = "c"+num4;
 
-    credits -= 10
-    jackpot += 10
-
     updateCredits();
     updateJackpot();
     
-
+    if(num1 == num2 && num2 == num3 && num3 == num4){
+        return result = true;
+        credits += jackpot;
+        jackpot = 0;
+    }
+    else{
+        return result = false
+    }
 }
 
+//Gathers result of Game 
+const getresult = () =>{
+    let resultBox = document.getElementById("result")
+    if(result){
+        resultBox.innerHTML="Congratulations you have won the Jackpot"
+    }
+    else{
+        resultBox.innerHTML="Better luck next time!"
+    }
+    
+}
+
+//Checks if enough creidts to play the Game
 const spin = () =>{
+    let resultBox = document.getElementById("result")
+    if(credits >= 10){
+        credits -= 10
+        jackpot += 10
+        playGame();
+        getresult();
+    }
+    else{
+        resultBox.innerHTML="Not Enough Credits"
+    }
 
 }
