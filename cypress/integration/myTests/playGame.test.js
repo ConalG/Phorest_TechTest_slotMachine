4ef43b2efcc816169b8e924e2a513c76a96296fe
ConalG <conal-glynnmartin@hotmail.com>
2022-03-21 18:25:38 +0000

    Update to playGame.test.js
    Added Payout test

--- cypress/integration/myTests/playGame.test.js
+++ cypress/integration/myTests/playGame.test.js
@@ -3,79 +3,86 @@
 describe('Play Game', () => {
 
     beforeEach(() => {
-        cy.viewport(1280,720)
-        cy.visit('https://conalg.github.io/Phorest_TechTest_slotMachine/')
+        cy.viewport(1280,720);
+        cy.visit('https://conalg.github.io/Phorest_TechTest_slotMachine/');
     })
     
     it('Visit Site',() => {
-        cy.contains('We have given you 100 credits on the house!')
+        cy.contains('We have given you 100 credits on the house!');
 
-        cy.get('button#play').click()
+        cy.get('button#play').click();
         
         cy.get('span#result').invoke('text').then((text) => {
-            expect(text).equal('Spinning')
+            expect(text).equal('Spinning');
         })
-        cy.wait(5000)
+        cy.wait(5000);
         cy.get('span#result').invoke('text').then((text) => {
             if(text.includes('Congratulations you have won the Jackpot:')){
-                console.log('Game was won')
+                console.log('Game was won');
             }
             else if (text === 'Better luck next time!'){
-                console.log(`Game was lost`)
+                console.log(`Game was lost`);
             }
             else{
                 console.log(text);
                 throw ("cannot find any of them");
             }
-        }) 
+        }) ;
 
-    })
+    });
 
     it('Play until no credits',() =>{
         for( let i = 0; i < 10; i ++){
-            cy.get('button#play').click()
+            cy.get('button#play').click();
             cy.get('span#result').invoke('text').then((text) => {
-                expect(text).equal('Spinning')
+                expect(text).equal('Spinning');
             })
-            cy.wait(5000)
+            cy.wait(5000);
             cy.get('span#result').invoke('text').then((text) => {
                 if(text.includes('Congratulations you have won the Jackpot:')){
-                    console.log('Game was won')
+                    console.log('Game was won');
                 }
                 else if (text === 'Better luck next time!'){
-                    console.log(`Game was lost`)
+                    console.log(`Game was lost`);
                 }
                 else{
                     console.log(text);
                     throw ("cannot find any of them");
                 }
-            }) 
-        }
-        cy.get('button#play').click()
+            });
+        };
+        cy.get('button#play').click();
         cy.get('span#result').invoke('text').then((text) => {
-            expect(text).equal('Not Enough Credits')
-        })
-    })
+            expect(text).equal('Not Enough Credits');
+        });
+    });
     
     it('Check Balance',() => {
-        cy.get('button#balance').click()
+        cy.get('button#balance').click();
         
-        cy.get('span#result').contains('Your Balance is:')
+        cy.get('span#result').contains('Your Balance is:');
        
-    })
+    });
 
     it('CurrentJackpot',() => {
-        cy.get('button#checkJackpot').click()
+        cy.get('button#checkJackpot').click();
         
-        cy.get('span#result').contains('Current Jackpot is:')
+        cy.get('span#result').contains('Current Jackpot is:');
        
-    })
+    });
 
     it('Add 100 Credits',() => {
-        cy.get('button#add').click()
+        cy.get('button#add').click();
         
-        cy.get('span#result').contains('100 Credits Added')
+        cy.get('span#result').contains('100 Credits Added');
        
-    })
+    });
+
+    it('Payout',() => {
+        cy.get('button#payout').click();
+        
+        cy.get('span#result').contains('You have received as a payout:');
+       
+    });
     
-})
+});
